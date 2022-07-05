76bd2106e3a7e0b50701fa9b94809200692ac4d6
Joe Racz <raczonline@gmail.com>
2022-07-05 16:47:55 -0400

    fixing some mobile styling on bullets"

--- src/pages/home/partials/Bullets.vue
+++ src/pages/home/partials/Bullets.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
+    import { toRefs } from 'vue';
+
     interface Bullets {
         Items: { info: string }[]
     }
 
-    const props = defineProps({
-        content: Object
-    })
+    type Content = {
+        Items: { info: string }[]
+    }
+
+    interface Props {
+        content: Content;
+    }
+
+    const props = defineProps<Props>();
 </script>
 
 <template>
@@ -13,7 +21,7 @@
         aria-label="Promises section" 
         class="min-h-64 pt-[24rem] pb-48 px-32 flex flex-col bg-fa-light-blue"
     >   
-        <ul class="flex justify-between gap-8 relative z-20">
+        <ul class="flex justify-between gap-8 relative z-20 flex-wrap">
             <li 
                 v-for="(bullet, index) in props.content.Items" 
                 :key="`bullet-${index}`" 
