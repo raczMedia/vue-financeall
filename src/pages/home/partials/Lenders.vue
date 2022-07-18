d34923382c630d9a98d5c48d62d86d6b0553f63d
Joe Racz <raczonline@gmail.com>
2022-07-18 00:19:58 -0400

    final fixed for lenders ipad

--- src/pages/home/partials/Lenders.vue
+++ src/pages/home/partials/Lenders.vue
@@ -23,9 +23,15 @@
 <template>
     <section 
         aria-label="Lenders section" 
-        class="relative text-white min-h-64 pt-24 pb-12 px-8 lg:px-32 flex flex-col -mt-12"
+        class="
+            relative text-white min-h-64 pt-24 pb-12 px-8 flex flex-col -mt-12
+            lg:px-32
+        "
     >
-        <div class="absolute z-0 inset-0 -top-12 lg:top-0 clip-top-up-right bg-fa-blue overflow-hidden">
+        <div class="
+            absolute z-0 inset-0 -top-12 clip-top-up-right bg-fa-blue overflow-hidden
+            lg:top-0
+        ">
             <div class="absolute z-0 clip-right-up-right bg-gray-400/5 left-0 top-0 w-2/3 h-full"></div>
             <div class="absolute z-0 clip-right-up-right bg-gray-400/5 left-0 top-0 w-2/4 h-full"></div>
         </div>
@@ -34,20 +40,50 @@
             {{ props.content.title }}
         </p>
         
-        <section class="z-10 flex justify-between mt-4 flex-wrap lg:flex-nowrap">
+        <section 
+            class="
+                z-10 flex justify-between mt-4 flex-wrap 
+                lg:flex-nowrap
+            "
+        >
             <template v-if="content">
-                <article v-for="(item, index) in props.content.items" :key="`lender-${index}`" class="lg:w-auto w-1/2">
-                    <img :src="item.filename" alt="rbc" class="w-full">
+                <article 
+                    v-for="(item, index) in props.content.items" 
+                    :key="`lender-${index}`" 
+                    class="
+                        w-1/2
+                        lg:w-auto
+                    "
+                >
+                    <img :src="item.filename" class="w-full">
                 </article>
             </template>
         </section>
 
-        <div class="relative flex flex-col lg:flex-row lg:justify-between mt-8 lg:mt-32 items-start w-full z-10 lg:px-auto px-4">
-            <article class="lg:border-l-4 border-white lg:pl-3 mt-8 lg:w-1/5 pb-48 lg:pb-0 text-5xl lg:text-4xl text-center lg:text-left leading-normal font-semibold lg:font-normal lg:leading-[48px]">
+        <div class="
+            relative flex flex-col mt-8 items-start w-full z-10 px-4
+            lg:flex-row lg:justify-between lg:mt-32 lg:px-auto md:items-center
+        ">
+            <article class="
+                border-white pb-48 text-5xl text-center leading-normal font-semibold 
+                lg:pb-0 lg:text-left lg:border-l-4 lg:pl-3 mt-8 lg:w-1/5 lg:text-4xl lg:font-normal lg:leading-[48px]
+            ">
                 {{ props.content.slogan }}
             </article>
-            <img :src="props.content.sloganImage.filename" alt="rbc" class="hidden lg:block absolute right-0 -top-8 w-4/5">
-            <img :src="props.content.sloganImageSmall.filename" alt="rbc" class="lg:hidden block absolute right-0 -bottom-64 w-full">
+            <img 
+                :src="props.content.sloganImage.filename" 
+                class="
+                    hidden absolute right-0 -top-8 w-4/5
+                    lg:block
+                "
+            />
+            <img 
+                :src="props.content.sloganImageSmall.filename" 
+                class="
+                    block absolute right-0 -bottom-64 w-full 
+                    lg:hidden md:w-3/5 md:-bottom-[22rem] md:self-center md:right-auto
+                "
+            />
         </div>
     </section>
 </template>
