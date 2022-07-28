2f6a0585b49b5ff2861802b8c2610abac879120a
Joe Racz <raczonline@gmail.com>
2022-07-27 23:35:17 -0400

    removing resources from routes file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../pages/home/home.vue";
 import Inventory from "../pages/inventory/inventory.vue";
-import Resources from "../pages/resources/resources.vue";
 import FreeCreditCheck from "../pages/free-credit-check/free-credit-check.vue";
 import ValueMyTradeIn from "../pages/value-my-trade-in/value-my-trade-in.vue";
 
@@ -15,11 +14,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/inventory",
     name: "Inventory",
     component: Inventory,
-  },
-  {
-    path: "/resources",
-    name: "Resources",
-    component: Resources,
   },
   {
     path: "/value-my-trade-in",
