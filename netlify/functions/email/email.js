0df3b3ee79992ada57a2f2738a8b5cb0547a34fa
Joe Racz <raczonline@gmail.com>
2023-01-30 23:20:06 -0500

    commenting out email js

--- netlify/functions/email/email.js
+++ netlify/functions/email/email.js
@@ -1,41 +1,41 @@
-const client = require('@sendgrid/mail');
-const {
-    SENDGRID_API_KEY,
-    SENDGRID_TO_EMAIL,
-    SENDGRID_FROM_EMAIL,
-} = process.env;
+// const client = require('@sendgrid/mail');
+// const {
+//     SENDGRID_API_KEY,
+//     SENDGRID_TO_EMAIL,
+//     SENDGRID_FROM_EMAIL,
+// } = process.env;
 
-const handler = async function (event) {
-    const headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        'Access-Control-Allow-Credentials': true
-    }
+// const handler = async function (event) {
+//     const headers = {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "Content-Type",
+//         'Access-Control-Allow-Credentials': true
+//     }
 
-    const { message, email, firstName, lastName } = JSON.parse(event.body);
-    client.setApiKey(SENDGRID_API_KEY);
+//     const { message, email, firstName, lastName } = JSON.parse(event.body);
+//     client.setApiKey(SENDGRID_API_KEY);
 
-    const data = {
-        to: SENDGRID_TO_EMAIL,
-        from: SENDGRID_FROM_EMAIL,
-        subject: `Portfolio Site message from ${firstName} ${lastName} (${email})`,
-        html: message,
-    };
+//     const data = {
+//         to: SENDGRID_TO_EMAIL,
+//         from: SENDGRID_FROM_EMAIL,
+//         subject: `Portfolio Site message from ${firstName} ${lastName} (${email})`,
+//         html: message,
+//     };
 
-    try {
-        await client.send(data);
-        return {
-            statusCode: 200,
-            headers,
-            body: 'Message sent',
-        };
-    } catch (error) {
-        return {
-            statusCode: error.code,
-            headers,
-            body: JSON.stringify({ ...error }),
-        };
-    }
-};
+//     try {
+//         await client.send(data);
+//         return {
+//             statusCode: 200,
+//             headers,
+//             body: 'Message sent',
+//         };
+//     } catch (error) {
+//         return {
+//             statusCode: error.code,
+//             headers,
+//             body: JSON.stringify({ ...error }),
+//         };
+//     }
+// };
 
-module.exports = { handler }
+// module.exports = { handler }
