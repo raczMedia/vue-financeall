a953f87eba0738b4ec3e5d0cff59a25fb57fbb53
Joe Racz <raczonline@gmail.com>
2023-04-02 01:46:18 -0400

    info circle icon

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import './css/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faMapPin, faMobileAlt, faAt, faChevronDown, faArrowLeft, faArrowRight, faCheck, faEnvelope, faPencil } from '@fortawesome/free-solid-svg-icons'
+import { faMapPin, faMobileAlt, faAt, faChevronDown, faArrowLeft, faArrowRight, faCheck, faEnvelope, faPencil, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faFacebook, faLinkedin, faTwitter, faMapPin, faMobileAlt, faAt, faChevronDown, faArrowLeft, faArrowRight, faCheck, faEnvelope, faPencil)
+library.add(faFacebook, faLinkedin, faTwitter, faMapPin, faMobileAlt, faAt, faChevronDown, faArrowLeft, faArrowRight, faCheck, faEnvelope, faPencil, faInfoCircle)
 
 const app = createApp(App)
 
