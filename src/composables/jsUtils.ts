5d36d4fa057fe93d93d5afad20726102f798a71b
Joe Racz <raczonline@gmail.com>
2023-04-04 02:04:16 -0400

    beginning of common js utils

---
+++ src/composables/jsUtils.ts
@@ -0,0 +1,8 @@
+export const toSnakeCase = (str: string) => str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)!
+    .map(x => x.toLowerCase())
+    .join('_');
+
+export interface LooseObject {
+  [key: string]: any
+}
