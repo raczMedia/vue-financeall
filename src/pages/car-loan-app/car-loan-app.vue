7f269dd5c8309762c33a540f49a1cf16695d8134
Joe Racz <raczonline@gmail.com>
2023-04-20 12:12:26 -0400

    removing individual draft calls

--- src/pages/car-loan-app/car-loan-app.vue
+++ src/pages/car-loan-app/car-loan-app.vue
@@ -3,9 +3,10 @@
   import { StoryblokStateType } from '@/composables/storyblok/storyblok.types';
   import FinanceApplication from '@/components/FinanceApplication.vue';
 
-  const { state, content }: StoryblokStateType = await useStoryblokState("car-loan-application", 'draft');
+  const { state, content }: StoryblokStateType = await useStoryblokState("car-loan-application");
   useBridge(state);
 </script>
+
 <template>
   <FinanceApplication :content="content" />
 </template>
