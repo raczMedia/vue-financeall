43b05450dff8e9a8c6b986fd4e6f47025bda29d6
Joe Racz <raczonline@gmail.com>
2024-10-02 15:55:40 -0400

    adding temp url

--- src/pages/inventory/inventory.vue
+++ src/pages/inventory/inventory.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <section class="px-8 lg:px-32 py-32">
-        <iframe src="https://financifi.dealerpage.ca/" frameborder="0"></iframe>
-    </section>
+	<section class="px-8 lg:px-32 py-32">
+		<iframe src="https://demo.dealersite.cloud/inventory/" frameborder="0"></iframe>
+	</section>
 </template>
