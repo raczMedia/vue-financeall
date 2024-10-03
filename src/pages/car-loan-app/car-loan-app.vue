1e050c70fe75aeea78e37a4f261478763a5e59ca
Joe Racz <raczonline@gmail.com>
2024-10-03 14:35:38 -0400

    emptying car loan app

--- src/pages/car-loan-app/car-loan-app.vue
+++ src/pages/car-loan-app/car-loan-app.vue
@@ -1,10 +1,5 @@
 <script lang='ts' setup>
-  import { useBridge, useStoryblokState } from '@/composables/storyblok/storyblokComposable';
-  import { StoryblokStateType } from '@/composables/storyblok/storyblok.types';
-  import FinanceApplication from '@/components/FinanceApplication.vue';
 
-  const { state, content }: StoryblokStateType = await useStoryblokState("car-loan-application");
-  useBridge(state);
 </script>
 
 <template>
