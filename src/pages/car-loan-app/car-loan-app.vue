e3f987735be7d4754552110be00fbc62b0ad26c0
Joe Racz <raczonline@gmail.com>
2024-10-08 00:56:24 -0400

    update to car loan page

--- src/pages/car-loan-app/car-loan-app.vue
+++ src/pages/car-loan-app/car-loan-app.vue
@@ -3,7 +3,43 @@
 </script>
 
 <template>
-  <section class="py-32">
-    <div class="AskAva-embed" data-product="creditTool"></div>
+  <section class="py-32 px-8 lg:px-32 flex gap-16">
+    <div class="w-1/3">
+      <h1 class=" text-3xl font-bold">
+        Drive Your Perfect Car—We’ll Handle the Financing!
+      </h1>
+      <div class="w-full flex flex-col gap-4 p-4 shadow-lg bg-gray-200 mt-4">
+        <p>
+          At Financifi, getting the car you want is easy and hassle-free. With fast approvals, flexible terms, and options for every credit situation, we make owning & financing your new or used vehicle effortless. 
+        </p>
+  
+        <h2 class="text-xl mt-2 font-semibold">
+          Why You’ll Love Financing with Us:
+        </h2>
+        <ul>
+          <li>Super-Fast Approvals: Our quick online application gets the ball rolling.</li>
+          <li>Custom Terms: Flexible payments designed for your budget.</li>
+          <li>Low Rates, Big Savings: Enjoy competitive rates that keep your payments manageable.</li>
+          <li>Credit Issues? We’ve Got You!: Whether your credit is great, so-so, or in need of help, we’ll find the best solution for you.</li>
+        </ul>
+  
+        <h2 class="text-xl mt-2 font-semibold">
+          How it works:
+        </h2>
+        <ol>
+          <li>Quick Online Application: Apply in minutes from anywhere.</li>
+          <li>Fast Approvals or Pre-Approvals: Get matched with the best financing options.</li>
+          <li>Choose Your Car: Explore our range of new and used vehicles.</li>
+          <li>Drive Off: Sign and hit the road!</li>
+        </ol>
+  
+        <p>
+          Ready to hit the road? Apply now and drive away in the car you deserve!
+        </p>
+      </div>
+    </div>
+    <div class="w-2/3 border-l-8 border-fa-blue p-2 rounded-lg">
+      <div class="AskAva-embed" data-product="creditTool"></div>
+    </div>
   </section>
 </template>
