3e7b535d682637def12f5425935f47ddf8a20dd8
Joe Racz <raczonline@gmail.com>
2024-10-08 12:46:40 -0400

    inventory spacing

--- src/pages/inventory/inventory.vue
+++ src/pages/inventory/inventory.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-	<section class="pt-[5rem] pb-2 bg-white">
+	<section class="pt-[5rem] pb-2 bg-white px-12">
 		<iframe 
 			class="w-full min-h-screen" 
 			src="https://financifi.dealerpage.ca/" 
