ca4266aa7e04b49df3fc50a04bab0a5863cb8d66
Joe Racz <raczonline@gmail.com>
2025-01-26 23:47:18 -0500

    dropdown updated

---
+++ src/components/elements/elements.ts
@@ -0,0 +1,5 @@
+export type OptionType = {
+  title: string;
+  value: string;
+  isHeading?: boolean;
+}
--- src/components/elements/v-dropdown.vue
+++ src/components/elements/v-dropdown.vue
@@ -1,33 +1,71 @@
 <script lang="ts" setup>
-  import { nextTick, onMounted, ref } from 'vue'
+  import { nextTick, onMounted, ref, computed } from 'vue'
   import { onClickOutside } from '@vueuse/core'
+  import { OptionType } from './elements'
 
   const open = ref(true)
   const optionsContainer = ref()
   const dropdown = ref()
   const width = ref(0)
 
-  const props = defineProps<{
-    options: {value: string, title: string}[],
-    modelValue: {value: string, title: string},
-    buttonClass?: string
-  }>()
+  const props = withDefaults(defineProps<{
+    options: OptionType[],
+    modelValue: OptionType,
+    buttonClass?: string,
+    direction: 'left' | 'right' | 'down'
+  }>(), {
+    direction: 'down',
+  });
   const emit = defineEmits(['update:modelValue'])
 
-  const selectOption = (option) => {
+  const selectOption = (option: OptionType) => {
     emit('update:modelValue', option)
     open.value = !open.value
   }
   const toggleOpen = async () => {
     open.value = !open.value
+
     if (! width.value) {
       await nextTick()
       width.value = optionsContainer?.value?.clientWidth
     }
   }
 
+  const buttonClass = computed(() => open.value ? 'bg-blue-600' : 'bg-blue-500')
+
   onMounted(toggleOpen)
   onClickOutside(dropdown, () => open.value = false)
+
+  const baseClasses = computed(() => [
+    'absolute z-10',
+    'shadow-xl border border-gray-300 overflow-hidden rounded-lg mt-1 bg-white',
+    'transition-all duration-200 ease-in-out'
+  ])
+
+  const directionClasses = computed(() => ({
+    'origin-top': props.direction === 'down',
+    'origin-right': props.direction === 'right',
+    'origin-left': props.direction === 'left',
+    'top-0 right-2': props.direction === 'right',
+    'top-0 left-6': props.direction === 'left',
+    'top-full right-0': props.direction === 'down'
+  }))
+
+  const animationClasses = computed(() => {
+    const isHorizontal = props.direction === 'left' || props.direction === 'right';
+    const value = !open.value ? 0 : 100;
+    return [
+      `scale-${isHorizontal ? 'x' : 'y'}-${value}`,
+      `opacity-${value}`,
+      `min-w-${width.value ? 'full' : '0'}`
+    ];
+  })
+
+  const optionsContainerClass = computed(() => [
+    ...baseClasses.value,
+    directionClasses.value,
+    animationClasses.value
+  ])
 </script>
 
 <template>
@@ -39,7 +77,7 @@
           transition-all duration-800
           text-white rounded focus:outline-none select-none
         " 
-        :class="open ? 'bg-blue-600' : 'bg-blue-500'"
+        :class="buttonClass"
         @click="toggleOpen()"
       >
         <span class="text-left" :style="{'min-width': `${width || 0}px`}">
@@ -50,20 +88,16 @@
     </slot>
     <div 
       ref="optionsContainer"
-      class="
-        absolute z-10 left-0
-        shadow-lg border rounded-lg mt-1 bg-white 
-        transition-all duration-200 ease-in-out
-      "
-      :class="{
-        'top-0 scale-y-0 opacity-0': !open,
-        'top-full scale-y-100 opacity-100': open,
-        'min-w-full': width
-      }"
+      :class="optionsContainerClass"
     >
       <template v-for="option in props.options">
-        <div 
-          class="cursor-pointer py-1 px-2 hover:bg-blue-100 whitespace-nowrap" 
+        <template v-if="option.isHeading">
+          <div class="p-2 whitespace-nowrap bg-gray-100 text-gray-400 italic select-none">
+            {{ option.title }}
+          </div>
+        </template>
+        <div v-else 
+          class="cursor-pointer border-b border-gray-200 px-4 p-2 hover:bg-blue-100 whitespace-nowrap select-none" 
           @click="selectOption(option)"
         >
           {{ option.title }}
